91a9d2d6585dd294ce66f9dee38d4170d6a8adcb
MartAna <92936649+MartAna@users.noreply.github.com>
2021-11-21 18:12:53 +0300

    counter

--- app/src/main/java/com/example/superembroiderer/ActivityList.kt
+++ app/src/main/java/com/example/superembroiderer/ActivityList.kt
@@ -3,9 +3,6 @@ package com.example.superembroiderer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private const val LAST_SELECTED_ITEM = "LAST_SELECTED_ITEM"
